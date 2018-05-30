a6283aa4dcd3a7973c96a4898645e3c5f916fffc
Shanmuga S Nuthalapati <ssnuthalapati@gmail.com>
2018-05-30 14:01:08 -0700

    update the sample to use registered app cliendID

--- node-server/config.js
+++ node-server/config.js
@@ -1,6 +1,7 @@
  // Don't commit this file to your public repos. This config is for first-run
  exports.creds = {
     mongoose_auth_local: 'mongodb://localhost/tasklist', // Your mongo auth uri goes here
+    clientID: '5005adad-2444-4d95-a0ee-580be002ce1c'
     identityMetadata: 'https://login.microsoftonline.com/TMOAAD.onmicrosoft.com/.well-known/openid-configuration', // This is customized for your tenant.
     // You may use the common endpoint for multi-tenant scenarios
     // if you do, make sure you set validateIssuer to false and specify an audience
