9de2a76c50109238784920c980ad9ffd54a5aa16
Shanmuga S Nuthalapati <ssnuthalapati@gmail.com>
2018-05-31 12:25:58 -0700

    added Users & owner values in app.js and mongodb related config details in config.js

--- node-server/config.js
+++ node-server/config.js
@@ -4,18 +4,19 @@
     clientID: '5005adad-2444-4d95-a0ee-580be002ce1c',
     // Required, the reply URL registered in AAD for your app
     redirectUrl: 'http://localhost:3000/auth/openid/return',
-    identityMetadata: 'https://login.microsoftonline.com/tmoaad.onmicrosoft.com/.well-known/openid-configuration', // This is customized for your tenant.
+    //identityMetadata: 'https://login.microsoftonline.com/tmoaad.onmicrosoft.com/.well-known/openid-configuration', // This is customized for your tenant.
     // You may use the common endpoint for multi-tenant scenarios
     // if you do, make sure you set validateIssuer to false and specify an audience
     // as you won't get this from the Identity Metadata
     //
-    //identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration',
-    validateIssuer: true, // if you have validation on, you cannot have users from multiple tenants sign in
+    identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration',
+    validateIssuer: false, // if you have validation on, you cannot have users from multiple tenants sign in
+    audience: 'https://TMOAAD.onmicrosoft.com/NodeWebAPI', // the Client ID of the application that is calling your API, usually a web API or native client
     passReqToCallback: false,
     loggingLevel: 'info' // valid are 'info', 'warn', 'error'. Error always goes to stderr in Unix.
  };
 
- // If you want to use the mongoDB session store for session middleware; otherwise we will use the default
+// If you want to use the mongoDB session store for session middleware; otherwise we will use the default
 // session store provided by express-session.
 // Note that the default session store is designed for development purpose only.
 exports.useMongoDBSessionStore = true;
